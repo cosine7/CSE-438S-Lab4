47e0127edea11a7eee2f10df9ab3a7e1ec4d570d
ChenXiao Liu <810196813@qq.com>
2021-10-25 15:03:43 -0500

    add youtube dependency

---
+++ ChenXiaoLiu-Lab4/VideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoViewController.swift
+//  ChenXiaoLiu-Lab4
+//
+//  Created by lcx on 2021/10/25.
+//
+
+import UIKit
+//import youtube_ios_player_helper
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-ChenXiaoLiu-Lab4/Pods-ChenXiaoLiu-Lab4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChenXiaoLiu_Lab4 : NSObject
+@end
+@implementation PodsDummy_Pods_ChenXiaoLiu_Lab4
+@end
